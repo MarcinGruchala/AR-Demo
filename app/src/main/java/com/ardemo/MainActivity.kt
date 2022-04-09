b8d191a1e35951e5a1036f74873e5bb396e4f000
Marcin Gruchala <margruchala@gmail.com>
2022-04-10 00:09:53 +0200

    Add color button

--- app/src/main/java/com/ardemo/MainActivity.kt
+++ app/src/main/java/com/ardemo/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.ardemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.scale
+import com.ardemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    binding.colorButton.setOnClickListener {
+      ViewCopier.copyViewToBitmap(
+        view = binding.sceneView,
+        onSuccess = { bitmap ->
+          val color = bitmap.scale(1, 1).getColor(0, 0).toArgb()
+          it.setBackgroundColor(color)
+        },
+        onError = {
+          //todo
+        }
+      )
+    }
   }
 }
