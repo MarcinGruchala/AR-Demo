59c0b00977804ea0df8d95b6dfc4593920428f71
Marcin Gruchala <margruchala@gmail.com>
2022-04-10 10:46:37 +0200

    Add object button with basic object placement

--- app/src/main/java/com/ardemo/MainActivity.kt
+++ app/src/main/java/com/ardemo/MainActivity.kt
@@ -1,18 +1,63 @@
 package com.ardemo
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.scale
+import androidx.lifecycle.lifecycleScope
 import com.ardemo.databinding.ActivityMainBinding
+import dev.romainguy.kotlin.math.Float3
+import io.github.sceneview.ar.node.ArModelNode
+import io.github.sceneview.ar.node.PlacementMode.INSTANT
 
 class MainActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivityMainBinding
 
+  private lateinit var modelNode: ArModelNode
+
+  private var isObjectPresent = false
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
+
+    modelNode = ArModelNode(placementMode = INSTANT)
+
+    lifecycleScope.launchWhenStarted {
+      modelNode.apply {
+        loadModel(
+          context = applicationContext,
+          glbFileLocation = "models/FiatPunto.glb",
+          autoAnimate = true,
+          autoScale = true,
+          centerOrigin = null
+        )
+        scale(0.5f)
+        placementPosition = Float3(0.0f, 0.0f, -1.0f)
+      }
+    }
+
+    binding.sceneView.onArFrame = { arFrame ->
+      if (arFrame.session.isTrackingPlane) {
+        binding.objectButton.visibility = View.VISIBLE
+      }
+    }
+
+    binding.objectButton.setOnClickListener {
+      if (isObjectPresent) {
+        binding.sceneView.removeChild(modelNode)
+        modelNode.destroy()
+        binding.objectButton.text = "Place the object"
+        isObjectPresent = false
+      } else {
+        binding.sceneView.addChild(modelNode)
+        modelNode.anchor()
+        binding.objectButton.text = "Remove"
+        isObjectPresent = true
+      }
+    }
 
     binding.colorButton.setOnClickListener {
       ViewCopier.copyViewToBitmap(
@@ -27,4 +72,5 @@ class MainActivity : AppCompatActivity() {
       )
     }
   }
+
 }
