b17181196dafce7127a9b8befa98491d1588d711
Marcin Gruchala <margruchala@gmail.com>
2022-06-26 14:16:17 +0200

    migrate to navigation component

---
+++ app/src/main/java/com/ardemo/BaseBindingFragment.kt
@@ -0,0 +1,25 @@
+package com.ardemo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseBindingFragment<Binding : ViewDataBinding> : Fragment() {
+
+    abstract val layoutRes: Int
+
+    protected lateinit var binding: Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(layoutInflater, layoutRes, container, false)
+        return binding.root
+    }
+}
--- app/src/main/java/com/ardemo/MainActivity.kt
+++ app/src/main/java/com/ardemo/MainActivity.kt
@@ -15,102 +15,13 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : BaseBindingActivity<ActivityMainBinding>() {
 
-  override val layoutRes: Int =
-    R.layout.activity_main
+    override val layoutRes: Int =
+        R.layout.activity_main
 
-  @Inject
-  lateinit var modelNode: ArModelNode
 
-  @Inject
-  lateinit var resources: ApplicationResources
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-  @Inject
-  lateinit var utils: Utils
-
-  private var isObjectPresent = false
-
-  private var isPlaneDetected = false
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-
-    lifecycleScope.launchWhenStarted {
-      modelNode.apply {
-        loadModel(
-          context = applicationContext,
-          glbFileLocation = "models/FiatPunto.glb",
-          autoAnimate = false,
-          autoScale = true,
-          centerOrigin = null
-        )
-        scale(0.25f)
-        placementPosition = Float3(0.0f, 0.0f, -1.0f)
-      }
     }
 
-    binding.sceneView.onArFrame = { arFrame ->
-      processFrame(arFrame)
-    }
-
-    binding.objectButton.setOnClickListener(::objectButtonClicked)
-
-    binding.colorButton.setOnClickListener(::colorButtonClicked)
-  }
-
-  private fun processFrame(frame: ArFrame) {
-    when (frame.updatedTrackables.isNotEmpty()) {
-      true -> {
-        binding.objectButton.visibility = View.VISIBLE
-        isPlaneDetected = true
-        binding.planeIndicator.setImageResource(resources.green())
-      }
-      false -> {
-        isPlaneDetected = false
-        binding.planeIndicator.setImageResource(resources.red())
-      }
-    }
-  }
-
-  private fun objectButtonClicked(button: View) {
-    when (isObjectPresent) {
-      true -> removeObject()
-      false -> placeObject()
-    }
-  }
-
-  private fun placeObject() {
-    when (isPlaneDetected) {
-      true -> {
-        binding.sceneView.addChild(modelNode)
-        modelNode.anchor()
-        binding.objectButton.text = resources.removeButtonText()
-        isObjectPresent = true
-      }
-      false -> {
-        Toast.makeText(
-          applicationContext, resources.noPlaneMessage(), Toast.LENGTH_LONG)
-          .show()
-      }
-    }
-  }
-
-  private fun removeObject() {
-    binding.sceneView.removeChild(modelNode)
-    modelNode.destroy()
-    binding.objectButton.text = resources.placeObjectButton()
-    isObjectPresent = false
-  }
-
-  private fun colorButtonClicked(button: View) {
-    utils.copyViewToBitmap(
-      view = binding.sceneView,
-      onSuccess = { bitmap ->
-        val color = utils.calculateAverageColor(bitmap)
-        button.setBackgroundColor(color)
-      },
-      onError = {
-        Log.e("MainActivity", "Error during screen copping")
-      }
-    )
-  }
 }
